3cbb00cffc48dadb5f5b5b5004776140e3006dd2
acadaccount <seletskyi@outlook.com>
2017-08-09 19:47:16 +0200

    And now I commit project C#

---
+++ Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+       abstract  class Point
+       {
+           int Coord_X;
+           int Coord_Y;
+           int Coord_Z;
+       }
+        public class Decart :Point
+        {
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
