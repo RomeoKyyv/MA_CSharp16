aefd466fdff469a0d9dbeb70b2c00ea7707782f0
djserega <galyuk.sergey@gmail.com>
2017-08-23 19:16:00 +0300

    +2.3

---
+++ galyuk.sergey/2/2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Money.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    // 1) declare enumeration CurrencyTypes, values UAH, USD, EU
+    enum CurrencyTypes { UAH, USD, EU }
+
+    public static class RateCurrencies
+    {
+        public static double rateUAH = 1;
+        public static double rateUSD = 8;
+        public static double rateEU = 10;
+    }
+
+    class Money
+    {
+        // 2) declare 2 properties Amount, CurrencyType
+        public double Amount { get; set; }
+        public CurrencyTypes? CurrencyType { get; set; }
+        public double CurrentRate { get; set; }
+
+        // 3) declare parameter constructor for properties initialization
+        public Money(double amount, CurrencyTypes currencyType, double currentRate)
+        {
+            this.Amount = amount;
+            this.CurrencyType = currencyType;
+            this.CurrentRate = currentRate;
+        }
+
+        // 4) declare overloading of operator + to add 2 objects of Money
+        public static Money operator +(Money money1, Money money2)
+        {
+            double currentRate = GetCurrentRateCurrency(money1);
+
+            money1.Amount = money1.Amount + (money2.Amount * money2.CurrentRate / currentRate);
+
+            return money1;
+        }
+        public static Money operator +(Money money1, double amountPlus)
+        {
+            double currentRate = GetCurrentRateCurrency(money1);
+
+            money1.Amount = money1.Amount + (amountPlus * RateCurrencies.rateUAH / currentRate);
+
+            return money1;
+        }
+
+        // 5) declare overloading of operator -- to decrease object of Money by 1
+        public static Money operator -(Money money1, Money money2)
+        {
+            double currentRate = GetCurrentRateCurrency(money1);
+
+            money1.Amount = money1.Amount - (money2.Amount * money2.CurrentRate / currentRate);
+
+            return money1;
+        }
+
+        // 6) declare overloading of operator * to increase object of Money 3 times
+        public static Money operator *(Money money, int count)
+        {
+            if (count == 0)
+                count = 3;
+            return money.Amount * count;
+        }
+        public static Money operator /(Money money, int count)
+        {
+            if (count == 0)
+                count = 2;
+
+            return money.Amount / 3;
+        }
+
+
+        // 7) declare overloading of operator > and < to compare 2 objects of Money
+        public static bool operator >(Money money1, Money money2)
+        {
+            double currentRate1 = GetCurrentRateCurrency(money1);
+            double currentRate2 = GetCurrentRateCurrency(money2);
+
+            double amountUAH1 = money1.Amount * money1.CurrentRate;
+            double amountUAH2 = money2.Amount * money2.CurrentRate;
+
+            return amountUAH1 > amountUAH2;
+        }
+        public static bool operator <(Money money1, Money money2)
+        {
+            double currentRate1 = GetCurrentRateCurrency(money1);
+            double currentRate2 = GetCurrentRateCurrency(money2);
+
+            double amountUAH1 = money1.Amount * money1.CurrentRate;
+            double amountUAH2 = money2.Amount * money2.CurrentRate;
+
+            return amountUAH1 < amountUAH2;
+        }
+
+        // 8) declare overloading of operator true and false to check CurrencyType of object
+        public static bool operator true(Money money)
+        {
+            return money.CurrencyType != null;
+        }
+        public static bool operator false(Money money)
+        {
+            return money.CurrencyType == null;
+        }
+
+        // 9) declare overloading of implicit/ explicit conversion  to convert Money to double, string and vice versa
+        public static implicit operator double(Money money)
+        {
+            double currentRate = GetCurrentRateCurrency(money);
+            return money.Amount * currentRate;
+        }
+
+        public static implicit operator Money(double amount)
+        {
+            return new Money(amount, CurrencyTypes.UAH, RateCurrencies.rateUAH);
+        }
+
+        public static implicit operator Money(string amountString)
+        {
+            if (double.TryParse(amountString, out double amount))
+                return new Money(amount, CurrencyTypes.UAH, RateCurrencies.rateUAH);
+            else
+                new Exception("Error. Failed convert '" + amountString + "' to double.");
+
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return Convert.ToString(this);
+            //double currentRate = GetCurrentRateCurrency(this);
+            //return (Amount * currentRate).ToString();
+        }
+
+
+
+        private static double GetCurrentRateCurrency(Money money)
+        {
+            double currentRate;
+            switch (money.CurrencyType)
+            {
+                case CurrencyTypes.UAH:
+                    currentRate = RateCurrencies.rateUAH;
+                    break;
+                case CurrencyTypes.USD:
+                    currentRate = RateCurrencies.rateUSD;
+                    break;
+                case CurrencyTypes.EU:
+                    currentRate = RateCurrencies.rateEU;
+                    break;
+                default:
+                    currentRate = RateCurrencies.rateUAH;
+                    break;
+            }
+            return currentRate;
+        }
+
+    }
+}
---
+++ galyuk.sergey/2/2.3/CSharp_Net_module1_2_3_lab/CSharp_Net_module1_2_3_lab/Program.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_2_3_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Current currency rate:");
+            Console.WriteLine("USD: {0}", RateCurrencies.rateUSD);
+            Console.WriteLine("EU: {0}", RateCurrencies.rateEU);
+            Console.WriteLine("--------------------------------------------");
+
+            Console.WriteLine("Enter amount UAH:");
+            double amountUAH = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter amount USD:");
+            double amountUSD = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter amount EU:");
+            double amountEU= double.Parse(Console.ReadLine());
+
+            // 10) declare 2 objects
+            Money UAH = new Money(amountUAH, CurrencyTypes.UAH, RateCurrencies.rateUAH);
+            Money USD = new Money(amountUSD, CurrencyTypes.USD, RateCurrencies.rateUSD);
+            Money EU = new Money(amountEU, CurrencyTypes.EU, RateCurrencies.rateEU);
+
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Choice of action:");
+            Console.WriteLine("1 - add 2 objects of Money");
+            Console.WriteLine("2 - add 1st object of Money and double (in developing)");
+            Console.WriteLine("3 - decrease 2nd object of Money by 1 ");
+            Console.WriteLine("4 - increase 1st object of Money");
+            Console.WriteLine("5 - compare 2 objects of Money");
+            Console.WriteLine("6 - compare 2nd object of Money and string (in developing)");
+            Console.WriteLine("7 - check CurrencyType of every object");
+            Console.WriteLine("8 - convert 1st object of Money to string");
+            Console.WriteLine("--------------------------------------------");
+
+            int userChoice = int.Parse(Console.ReadLine());
+
+            switch (userChoice)
+            {
+                case 1:
+
+                    #region add 2 objects of Money
+
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("EU: {0}, {1}", EU.Amount, EU.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH + USD");
+                    UAH += USD;
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("USD + EU");
+                    USD += EU;
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    break;
+
+                    #endregion
+
+                case 2:
+
+                    #region add 1st object of Money and double
+
+                    break;
+
+                    #endregion
+
+                case 3:
+
+                    #region decrease 2nd object of Money by 1 
+
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("EU: {0}, {1}", EU.Amount, EU.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("USD - UAH");
+                    UAH -= USD;
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("EU - USD");
+                    USD -= EU;
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    break;
+
+                    #endregion
+
+                case 4:
+
+                    #region increase 1st object of Money
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("Enter amount UAH:");
+                    double amountPlus = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("EU: {0}, {1}", EU.Amount, EU.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH + {0}", amountPlus);
+                    UAH += amountPlus;
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("USD + {0}", amountPlus);
+                    USD += amountPlus;
+                    Console.WriteLine("UAH: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("EU + {0}", amountPlus);
+                    EU += amountPlus;
+                    Console.WriteLine("UAH: {0}, {1}", EU.Amount, EU.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    break;
+
+                    #endregion
+
+                case 5:
+
+                    #region compare 2 objects of Money
+
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH: {0}, {1}", UAH.Amount, UAH.CurrentRate);
+                    Console.WriteLine("USD: {0}, {1}", USD.Amount, USD.CurrentRate);
+                    Console.WriteLine("EU: {0}, {1}", EU.Amount, EU.CurrentRate);
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("UAH > USD");
+                    bool result = UAH > USD;
+                    Console.WriteLine("Result: {0}", result.ToString());
+                    Console.WriteLine("--------------------------------------------");
+                    Console.WriteLine("EU > USD");
+                    result = EU > USD;
+                    Console.WriteLine("Result: {0}", result.ToString());
+                    Console.WriteLine("--------------------------------------------");
+
+                    break;
+
+                    #endregion
+
+                case 6:
+
+                    #region compare 2nd object of Money and string
+
+                    break;
+
+                    #endregion
+
+                case 7:
+
+                    #region check CurrencyType of every object
+
+                    Console.WriteLine("currency type UAH = null? - {0}", UAH);
+                    Console.WriteLine("currency type USD = null? - {0}", USD);
+                    Console.WriteLine("currency type EU = null? - {0}", EU);
+                    break;
+
+                    #endregion
+
+                case 8:
+
+                    #region convert 1st object of Money to string
+
+                    Console.WriteLine("UAH to string: {0}", UAH.ToString());
+                    Console.WriteLine("USD to string: {0}", USD.ToString());
+                    Console.WriteLine("EU to string: {0}", EU.ToString());
+                    break;
+
+                    #endregion
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
