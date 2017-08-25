5034a378ec5f53bda7f7dbaebc9914321f19608d
Seletsky <seletskyi@outlook.com>
2017-08-25 18:26:57 +0200

    Добавил лабу с Events 1_4_1

---
+++ Seletsky/CSharp_Net_module1_4_1_lab_seletskyi/CSharp_Net_module1_4_1_lab/OnlineShop.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class OnlineShop
+    {
+        // 4) declare event of type EventHandler<GoodsInfoEventArgs>
+       public  event EventHandler<GoodsInfoEventArgs> EvNewGood;
+
+        // 5) declare method NewGoods for event initiation
+        // use parameter string to get GoodsName
+        public void  NewGoods(string GoodsName)
+        {
+
+            // don't forget to check if event is not null
+            // in true case intiate the event
+            // use next line
+            if (EvNewGood != null)
+            {
+                EvNewGood(this, new GoodsInfoEventArgs(GoodsName));
+                Console.WriteLine("your_event_name(this, new GoodsInfoEventArgs(GoodsName)");
+            }
+            // your_event_name(this, new GoodsInfoEventArgs(GoodsName));
+            else
+            {
+
+                Console.WriteLine("Net podpiskiiiiiiii"); 
+            }
+           
+        }
+    }
+}
---
+++ Seletsky/CSharp_Net_module1_4_1_lab_seletskyi/CSharp_Net_module1_4_1_lab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Net_module1_4_1_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 9) declare object of OnlineShop 
+            OnlineShop shop = new OnlineShop();
+            // 10) declare several objects of Customer
+            Customer c1 = new Customer("Kon");
+            Customer c2 = new Customer("Kon1");
+            Customer c3 = new Customer("Kon2");
+            // 11) subscribe method GotNewGoods() of every Customer instance 
+            // to event NewGoodsInfo of object of OnlineShop
+           // shop.EvNewGood += c1.GotNewGoods;
+            shop.EvNewGood += c3.GotNewGoods;
+
+            // 12) invoke method NewGoods() of object of OnlineShop
+            // discuss results
+            shop.NewGoods("Ball");
+            Console.ReadLine();
+        }
+    }
+}
