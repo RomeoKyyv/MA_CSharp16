d30a027a92bd45de98b82800362d4d54ac609d4c
Seletsky <seletskyi@outlook.com>
2017-08-28 19:24:23 +0200

    Лаба с "женериками"

---
+++ Seletsky/Hello_Generics_stud/Hello_Generics_stud/Base.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hello_Generics_stud
+{
+
+    public static class Curry_list
+    {
+
+        public static Func<TArg2, Func<TArg1, TResult>> Bnd<TArg1, TArg2, TResult>(this Func<TArg1, TArg2, TResult> f)
+        {
+            return (y) => ((x) => f(x, y));
+        }
+    }
+
+    class Base
+    {
+    }
+}
