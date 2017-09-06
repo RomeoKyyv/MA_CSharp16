cd00cfcfe87c57fa7a723af22c64adac7ffaf1b0
acadaccount <aoskiev@gmail.com>
2017-09-06 20:35:46 +0300

    sql project for ms sql express

---
+++ CodeFirstNewDatabaseSample/CodeFirstNewDatabaseSample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CodeFirstNewDatabaseSample.Model;
+
+namespace CodeFirstNewDatabaseSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BloggingContext())
+            {
+                Console.Write("Enter a name for a new Blog: ");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                context.Blogs.Add(blog);
+                context.SaveChanges();
+
+                var blogs = context.Blogs.OrderBy(b => b.Name);
+                //var query = from b in context.Blogs
+                //            orderby b.Name
+                //            select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (var item in blogs)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
