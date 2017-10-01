31264a69d81b6703caeaa4f2d9cf5f52920817d0
Seletsky <seletskyi@outlook.com>
2017-10-01 17:22:44 +0200

    Лаба с сериализацией в файлы

---
+++ Seletsky/Hello_Serialization_stud/Hello_Serialization_stud/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Xml.Serialization;
+
+namespace Hello_Serialization_stud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instance of Student class
+            Student One = new Student();
+            // Initialize its properties
+            One.FirstName = "JustName";
+            One.LastName = "JustSename";
+            One.Nationality = "OfUnivers";
+            One.SetAddress("Univers", "030303");
+            // Call methods for serialization and deserialization
+            BinaryFrm(One);
+            Console.ReadKey();
+            SoapFrm(One);
+            Console.ReadKey();
+            XmlFrm(One);
+            Console.ReadKey();
+        }
+
+        // Impement BinaryFrm(Student p) method to serialize and deserialize p
+        static public void BinaryFrm(Student p)
+        {        // Define path for file
+            string FileName = @"D:\\testlab2.bin";
+            Student student = new Student();
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.Nationality);
+
+            Console.WriteLine("********************************");
+            // Implement BinaryFormatter object creation and p serialization  in using block for FileStream object
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (Stream newStream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None)) 
+            {
+                binaryFormatter.Serialize(newStream, p);
+            }
+            // Implement BinaryFormatter object creation and  deserialization  in using block for FileStream object
+            using (Stream readStream = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+               student = (Student)binaryFormatter.Deserialize(readStream);
+            }
+            // Write deserialization result to console
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.LastName);
+            Console.WriteLine(student.Nationality);
+
+        }
+
+
+        // Impement SoapFrm(Student p) method to serialize and deserialize p
+        static public void SoapFrm(Student p)
+        {
+            string FileName = @"D:\\testlabFOAM.bin";
+            Student student = new Student();
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.Nationality);
+
+            Console.WriteLine("********************************");
+            // Implement BinaryFormatter object creation and p serialization  in using block for FileStream object
+            SoapFormatter SoapFormatter = new SoapFormatter();
+            using (Stream newStream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                SoapFormatter.Serialize(newStream, p);
+            }
+            // Implement BinaryFormatter object creation and  deserialization  in using block for FileStream object
+            using (Stream readStream = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                student = (Student)SoapFormatter.Deserialize(readStream);
+            }
+            // Write deserialization result to console
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.LastName);
+            Console.WriteLine(student.Nationality);
+
+        }
+        // Define path for file
+        // Implement SoapFormatter object creation and p serialization  in using block for FileStream object
+
+        // Implement SoapFormatter object creation and  deserialization  in using block for FileStream object
+        // Write deserialization result to console
+
+        // Impement XmlFrm(Student p) method to serialize and deserialize p 
+        static public void XmlFrm(Student p)
+         {
+            string FileName = @"D:\\testlabXML.xml";
+        Student student = new Student();
+        Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.Nationality);
+
+            Console.WriteLine("********************************");
+            // Implement BinaryFormatter object creation and p serialization  in using block for FileStream object
+            XmlSerializer xmlwrite = new XmlSerializer(typeof(Student));
+                
+            using (Stream newStream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                xmlwrite.Serialize(newStream, p);
+            }
+            // Implement BinaryFormatter object creation and  deserialization  in using block for FileStream object
+            using (Stream readStream = new FileStream(FileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                student = (Student) xmlwrite.Deserialize(readStream);
+            }
+            // Write deserialization result to console
+            Console.WriteLine(student.FirstName);
+            Console.WriteLine(student.LastName);
+            Console.WriteLine(student.Nationality);
+
+        }
+       
+        // Define path for file
+        // Create XmlSerializer serializer typeof Student       
+        // Implement  p serialization  in using block for FileStream object
+
+        // Create XmlSerializer deserializer typeof Student 
+        // Implement   deserialization  in using block for FileStream object
+        // Write deserialization result to console
+
+    }
+}
+
---
+++ Seletsky/Hello_Serialization_stud/Hello_Serialization_stud/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_Serialization_stud
+{
+
+    [Serializable] //Required by BinaryFormatter and SoapFormatter  
+    public class Student  //XMLSerializer needs the public class
+        {
+        [System.Xml.Serialization.XmlIgnore]   //Ignore in Xml Serialization          
+                                               //Public fields are serialize by the three formatters  
+        public string FirstName;
+        public string LastName;
+
+        [NonSerialized] //Thield will not be serialized
+
+        //These fields will not be serialized by XmlSerialization
+
+        public string Nationality;
+        private string Address;
+        private string Code;
+
+        // Create SetAddress(string address, string code) method
+        public void SetAddress(string address , string code)
+        {
+            Address = address;
+            Code = code;
+        }
+        // Override ToString() method
+        public override string ToString()
+        {
+            Console.WriteLine(this.FirstName);
+            return base.ToString();
+        }
+    }  
+    
+}
