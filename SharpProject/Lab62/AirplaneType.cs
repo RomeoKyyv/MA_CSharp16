8e39cdae7c2a9bded05c974d6b2053c49efaac70
o_oosipenko <aoskiev@gmail.com>
2017-11-01 20:35:09 +0200

    another lab 62 step

--- SharpProject/Lab62/AirplaneType.cs
+++ SharpProject/Lab62/AirplaneType.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
 using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace SharpProject.Lab62
 {
@@ -15,12 +20,66 @@ namespace SharpProject.Lab62
     {
     }
 
+    public class CheckSaveTrace
+    {
+        public void CheckClassAttribute(object input)
+        {
+            ProcessAttributes(input);
+        }
+
+        public void SaveTrace(object input)
+        {
+            WriteToFile("E:/temp/output.txt", ProcessAttributes(input).ToString());
+        }
+
+        public void EventLogging(object input)
+        {
+            var assemblyName = "Application";
+            if (!EventLog.SourceExists(assemblyName))
+            {
+                EventLog.CreateEventSource(assemblyName, "Application log");
+            }
+
+            EventLog log = new EventLog();
+            log.Source = assemblyName;
+            log.WriteEntry(ProcessAttributes(input).ToString());
+        }
+
+        private void WriteToFile(string path, string content)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            File.WriteAllText(path, content);
+        }
+
+        private StringBuilder ProcessAttributes(object input)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (Attribute attribute in input.GetType().GetCustomAttributes())
+            {
+                string name = "Scanning " + input.GetType().Name;
+                result.Append(name);
+                result.Append("\n");
+                Console.WriteLine(name);
+                foreach (var fieldInfo in attribute.GetType().GetFields())
+                {
+                    string field = fieldInfo.Name + " is " + fieldInfo.GetValue(attribute);
+                    result.Append(field);
+                    result.Append("\n");
+                    Console.WriteLine(field);
+                }
+            }
+            return result;
+        }
+    }
 
     public abstract class Aircraft
     {
         public Aircraft()
         {
-            Console.WriteLine(this.GetType().Name);
+            Console.WriteLine("This is " + this.GetType().Name);
         }
     }
 
--- SharpProject/Lab62/Lab62Launcher.cs
+++ SharpProject/Lab62/Lab62Launcher.cs
@@ -7,21 +7,11 @@ namespace SharpProject.Lab62
         public Lab62Launcher()
         {
             Console.WriteLine("Lunching plane...");
-            TraceLog(new CargoPlane());
-            Console.WriteLine("Lunching plane...");
-            TraceLog(new UniversalAirplane());
+            CheckSaveTrace trace = new CheckSaveTrace();
+            trace.CheckClassAttribute(new CargoPlane());
+            trace.SaveTrace(new CargoPlane());
+            trace.EventLogging(new CargoPlane());
             Console.WriteLine("Done....");
-        }
-
-        public void TraceLog(object input)
-        {
-            foreach (var attribute in input.GetType().GetCustomAttributes(false))
-            {
-                foreach (var fieldInfo in attribute.GetType().GetFields())
-                {
-                    Console.WriteLine(fieldInfo.Name + " is " + fieldInfo.GetValue(attribute));
-                }
-            }
         }
     }
 }
